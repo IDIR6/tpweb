a18d0af418a4e9721c1fedec78057194a7c2e0ac
IDIR6 <78723045+IDIR6@users.noreply.github.com>
2021-02-10 17:42:09 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -2,15 +2,18 @@ function showCity() {
 let nomVilleSelectionnee = document.getElementById("menu").value;
 	
  if (nomVilleSelectionnee == "Villé") 
- {document.getElementById("par").style.display = "none";
+ {document.getElementById("vil").style.display = "";
+document.getElementById("par").style.display = "none";
 document.getElementById("stras").style.display = "none";
  }
 else if (nomVilleSelectionnee == "Strasbourg") 
- {document.getElementById("par").style.display = "none";
+ {document.getElementById("stras").style.display = "";
+document.getElementById("par").style.display = "none";
 document.getElementById("vil").style.display = "none"; }
 
 else
- {document.getElementById("stras").style.display = "none";
+ {document.getElementById("par").style.display = "";
+document.getElementById("stras").style.display = "none";
 document.getElementById("vil").style.display = "none"; }
 	
 }	
