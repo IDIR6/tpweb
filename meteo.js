663be26bcfce78a0ba9c0732ccc38ac82d85eb0c
IDIR6 <78723045+IDIR6@users.noreply.github.com>
2021-02-10 16:51:13 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -1,1 +1,16 @@
-a
+function showCity() {
+let nomVilleSelectionnee = document.getElementById("menu").value;
+	
+ if (nomVilleSelectionnee == "Villé") 
+ {document.getElementById("par").style.display = "none";
+document.getElementById("stras").style.display = "none";
+ }
+else if (nomVilleSelectionnee == "Strasbourg") 
+ {document.getElementById("par").style.display = "none";
+document.getElementById("vil").style.display = "none"; }
+
+else
+ {document.getElementById("stras").style.display = "none";
+document.getElementById("vil").style.display = "none"; }
+	
+}	
